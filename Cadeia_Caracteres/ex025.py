ee29e18bcbc7385ef1fad90b9aeab0aa864c94cc
Roger Ernane de Almeida <69680625+Roger-0804@users.noreply.github.com>
2021-03-10 11:53:23 -0300

    Delete Cadeia_Caracteres directory

--- Cadeia_Caracteres/ex025.py
+++
@@ -1,7 +0,0 @@
-'''25. Crie um programa que leia o nome de uma pessoa e diga se ela tem  'Silva' no nome:'''
-
-nome=str(input('Qual seu nome:')).strip()
-print(nome[:5].upper() =='SILVA')
-
-
-
--- Cadeia_Caracteres/ex025a.py
+++
@@ -1,4 +0,0 @@
-'''25.Crie um programa que leia o nome de uma pessoa e diga se ela tem 'SILVA' no nome:'''
-
-nome=str(input('DIGITE SEU NOME:')).strip()
-print('O seu nome possui Silva? \n{}'.format('SILVA' in nome.upper()))
