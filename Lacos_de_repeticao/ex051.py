fe4f3da138adfd56b89a4438a8879cadb035cc73
Roger Ernane de Almeida <69680625+Roger-0804@users.noreply.github.com>
2021-03-10 11:55:39 -0300

    Delete Lacos_de_repeticao directory

--- Lacos_de_repeticao/ex051.py
+++
@@ -1,9 +0,0 @@
-'''51.Desenvolva um programa que leia o primeiro termo e a razão de uma PA .No final, mostre os 10 primeiros termos dessa progressão:'''
-
-print('\033[1;30m{:=^50}\033[m'.format('\033[1;32mProgressao Aritmética\033[m'))
-p1 = int(input('\033[7;30mDigite qual sera o primeiro termo da progressão:\033[m'))
-passo = int(input('Qual será a razão dessa progressão?'))
-decimo =  p1 + (10-1) * passo #Formula usada para calcular o décimo termo de uma progressão aritmética considerando  a razao.
-for c in range(p1,decimo+passo,passo):
-    print('{}'.format(c),end=' => ')
-print('Fim')
--- Lacos_de_repeticao/pratica01.py
+++
@@ -1,3 +0,0 @@
-for c in range(7,0,-3):
-    print(c)
-print('Fim')
