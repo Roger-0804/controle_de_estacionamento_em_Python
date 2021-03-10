2208f26e8c56e951eed0a06f8a2c51d86889d26b
Roger Ernane de Almeida <69680625+Roger-0804@users.noreply.github.com>
2021-03-10 11:53:45 -0300

    Delete Condicional em Python directory

--- Condicional em Python/ex030.py
+++
@@ -1,9 +0,0 @@
-'''30.Crie um programa que leia um numero inteiro e mostre se ele é par ou ímpar:'''
-
-print('-'*10,'Par ou Ímpar','-'*10)
-num=float(input('Digite um numero:'))
-res=num%2
-if res==0:
-    print('Par')
-else:
-    print('Ímpar')
--- Condicional em Python/teste.py
+++
@@ -1,5 +0,0 @@
-a='\033[7;30mAmanda''\033[m'
-b='\033[7;31;40mRoger''\033[m'
-c='\033[43;35mHelen''\033[m'
-
-print('Os nomes:{} e o {} e também  {} são muitos estranhos.'.format(a,b,c))
